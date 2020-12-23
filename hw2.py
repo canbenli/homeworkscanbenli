f5b1d51bc36ab8041a0e9e430c57f72d1f2a664b
canbenli <74454137+canbenli@users.noreply.github.com>
2020-12-23 14:33:35 +0300

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -7,8 +7,8 @@ liste = [a,b,x,c]
 
 for i in liste:
     if x < 18:
-        print("first name: {}\nlast name: {}\nage: {}\ndate of birth: {}\nyou cant go out cos its dangerous".format(a,b,x,c))
+        print("first name: {}\nlast name: {}\nage: {}\ndate of birth: {}\n***you cant go out cos its dangerous***".format(a,b,x,c))
     else:
-        print("first name: {}\nlast name: {}\nage: {}\ndate of birth: {}\nyou can go out".format(a,b,x,c))
+        print("first name: {}\nlast name: {}\nage: {}\ndate of birth: {}\n***you can go out***".format(a,b,x,c))
     break
 
